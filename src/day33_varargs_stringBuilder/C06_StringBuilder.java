3f67b57932a2298cc6ac577887f23f1eeeb89b51
Oner Tekin <tekinoner37@gmail.com>
2022-04-06 19:19:01 +0300

    06.04.2022

--- src/day33_varargs_stringBuilder/C06_StringBuilder.java
+++ src/day33_varargs_stringBuilder/C06_StringBuilder.java
@@ -7,7 +7,7 @@ public class C06_StringBuilder {
         System.out.println(sb1.insert(12," edin."));
 String str="Hayatta cok guzel seyler var";
         System.out.println(sb1.insert(0,str,0,8));
-        System.out.println(sb1.reverse());// ters cevirir.
+       System.out.println(sb1.reverse());// ters cevirir.
         System.out.println(sb1);
         System.out.println(sb1.reverse());
 
