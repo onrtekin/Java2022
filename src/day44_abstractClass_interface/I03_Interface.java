2c23807aac3fddb681e0b9b0b9a08ea5b543d5da
Oner Tekin <tekinoner37@gmail.com>
2022-04-14 19:15:39 +0300

    14.04.2022

---
+++ src/day44_abstractClass_interface/I03_Interface.java
@@ -0,0 +1,15 @@
+package day44_abstractClass_interface;
+
+public interface I03_Interface {
+    /*
+    tam soyutlama/full abstraction olusturmak istersek
+    abstract classlari degil interface kullaniriz
+
+    Interface lerde sadece abstract methodlar bulunur
+     */
+    public static final int SAYI = 20;
+    int SAYI2=40;
+
+
+
+}
