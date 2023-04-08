bf64efee8daf6dceeba0cf95d53b7430e63215f9
Oner TEKİN <tekinoner37@gmail.com>
2023-04-08 18:35:40 +0300

    deneme

--- src/day01_variables/C01_IlkClass.java
+++ src/day01_variables/C01_IlkClass.java
@@ -1,49 +1,16 @@
 package day01_variables;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Random;
+
 public class C01_IlkClass {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello World");
-
-        int sayi1=10;
-        int sayi2=20;
-        boolean erkekMi=true;
-        System.out.println(sayi1);
-
-        System.out.println(sayi2);
-
-        System.out.println(erkekMi);
-
-        String adi="nilufer";
-        int yasi=8;
-        char kac='5';
-
-        System.out.println(adi);
-        System.out.println(yasi);
-        System.out.println(kac);
-
-        boolean issizmi=true;
-
-        System.out.println(issizmi);
-
-        char kacagidiyor='2';
-
-        System.out.println(kacagidiyor);
-
-        double kacGram=2.22;
-
-        System.out.println(kacGram);
-
-
-
-
-
+        int x = 2;
+        System.out.println(++x - x++ - x-- + x++);
+        System.out.println(x);
 
 
     }
-
-
-
-
 }
